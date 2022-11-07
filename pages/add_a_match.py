03fccf09991240965f3e55db435c2eb381745a56
Iv-Ka <ivkacz14@gmail.com>
2022-11-07 17:20:41 +0100

    second_task

--- pages/add_a_match.py
+++ pages/add_a_match.py
@@ -12,3 +12,28 @@ class add_a_match(BasePage)
     Enemy_team_xpath = "//*[@name='enemyTeam']"
 
     My_team_score_xpath = "//*[@name='myTeamScore']"
+
+    date_xpath = "//*[@type='date']"
+
+    At_home_xpath = "// span[contains(text( ),'Match at home')]"
+
+    Out_home_xpath = "// span[contains(text( ),'Match out home')]"
+
+    T_shirt_xpath = "//*[@name='tshirt']"
+
+    league_xpath = "//*[@name='league']"
+
+    time_played_xpath = "//*[@name='timePlayed']"
+
+    number_xpath = "//*[@name='number']"
+
+    web_match_xpath = "//*[@name='webMatch']"
+
+    general_xpath = "//*[@name='general']"
+
+    rating_xpath = "//*[@name='rating']"
+
+
+
+
+
