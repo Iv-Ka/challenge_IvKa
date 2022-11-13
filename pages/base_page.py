b63560842196cdd571e117852d97e84725a823c6
Iv-Ka <ivkacz14@gmail.com>
2022-11-13 15:30:41 +0100

    third_task

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,9 @@
+import click
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class BasePage():
-
-    def __init__(self, driver: WebDriver):
-        self.driver = driver
 
     def field_send_keys(self, selector, value, locator_type=By.XPATH):
         return self.driver.find_element(locator_type, selector).send_keys(value)
--- pages/login_page.py
+++ pages/login_page.py
@@ -5,18 +5,18 @@ class LoginPage(BasePage):
     login_field_xpath = "//*[@id='login']"
     password_field_xpath = "//*[@id='password']"
     sign_in_button_xpath = "// button[contains(@type,'submit')]"
-
+    login_url = 'https://scouts-test.futbolkolektyw.pl/en/'
+    expected_title = "Scouts panel - sign in"
+    box_title_xpath = "//*[@id='__next']/form/div/div[1]/h5"
+    header_of_box = 'Scouts Panel'
 
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
 
-    def type_in_password(self, param):
-
-    user_login.type_in_password('Test-1234')
-        pass
-
+    def type_in_password(self, password):
+        self.field_send_keys(self.password_field_xpath, password)
 
     def click_on_the_sign_in_button(self):
+        self.click_on_the_element(self.sign_in_button_xpath)
 
-    user_login.click_on_the_sign_in_button()
-        pass
+    pass
