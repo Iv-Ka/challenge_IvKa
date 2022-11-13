17777c8bc2181dfdcd12a7b5de8e9f6b84ec7e47
Iv-Ka <ivkacz14@gmail.com>
2022-11-13 13:02:07 +0100

    third_task

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,5 +6,17 @@ class LoginPage(BasePage):
     password_field_xpath = "//*[@id='password']"
     sign_in_button_xpath = "// button[contains(@type,'submit')]"
 
+
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
+
+    def type_in_password(self, param):
+
+    user_login.type_in_password('Test-1234')
+        pass
+
+
+    def click_on_the_sign_in_button(self):
+
+    user_login.click_on_the_sign_in_button()
+        pass
